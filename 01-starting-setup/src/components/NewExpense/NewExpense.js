fd906103f4d2b78ca48b22bccf51abe8772eb694
minnnisu <mine52100@naver.com>
2022-11-25 20:44:44 +0900

    Section 5: 렌더링 리스트 및 조건부 Content 일부 학습

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,36 @@
-import react from "react";
+import react, { useState } from "react";
 import "./NewExpense.css";
 
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-  const saveExpenseData = (enterExpenseData) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const saveExpenseDataHandler = (enterExpenseData) => {
     const expenseData = {
       ...enterExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+    props.onAddExpenseData(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseData} />
+      {!isEditing && <button onClick={startEditingHandler}>Add button</button>}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
