f6de72b4d6215e5e34b578b252239b080121f2c8
minnnisu <mine52100@naver.com>
2022-12-09 22:59:32 +0900

    Section 15: 커스텀 리액트 Hook 빌드 학습 완료

---
+++ project-6/src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  console.log("refresh");
+  const [tasks, setTasks] = useState([]);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  // console.log("isLoding:" + isLoading);
+  // console.log("isError:" + error);
+
+  useEffect(() => {
+    const tranformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-4d4ca-default-rtdb.firebaseio.com/tasks.json",
+      },
+      tranformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ project-6/src/hooks/use-http.js
@@ -0,0 +1,32 @@
+import { useCallback, useState } from "react";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        response.text().then((msg) => alert(msg));
+        throw new Error("Request failed!");
+      }
+      applyData(data);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
+
+  return { isLoading, error, sendRequest };
+};
+
+export default useHttp;
